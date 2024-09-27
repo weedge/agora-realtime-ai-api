45781ba08ddd282931b86dfda3221ee961fe8d5a
zhangqianze <zhangqianze@agora.io>
2024-09-27 14:08:15 +0800

    initial commit

---
+++ token_builder/realtimekit_token_builder.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__copyright__ = "Copyright (c) 2014-2024 Agora.io, Inc."
+
+import logging
+logger = logging.getLogger(__name__)
+from .RtcTokenBuilder2 import RtcTokenBuilder
+
+class RealtimekitTokenBuilder(RtcTokenBuilder):
+    @staticmethod
+    def build_token(app_id:str, app_certificate:str, channel_name:str, uid:0, expiration_in_seconds:int = 3600):
+        """
+        Build the RTC token.
+        :return: The RTC token.
+        """
+        if not app_id :
+            raise ValueError("Need to set environment variable AGORA_APP_ID")
+        logger.info("App Id: %s" % app_id)
+        if not app_certificate :
+            return ""
+        token_expiration_in_seconds = expiration_in_seconds
+        join_channel_privilege_expiration_in_seconds = expiration_in_seconds
+        pub_audio_privilege_expiration_in_seconds = expiration_in_seconds
+        pub_video_privilege_expiration_in_seconds = expiration_in_seconds
+        pub_data_stream_privilege_expiration_in_seconds = expiration_in_seconds
+
+        token = RtcTokenBuilder.build_token_with_uid_and_privilege(
+            app_id, app_certificate, channel_name, uid, token_expiration_in_seconds,
+            join_channel_privilege_expiration_in_seconds, pub_audio_privilege_expiration_in_seconds, pub_video_privilege_expiration_in_seconds, pub_data_stream_privilege_expiration_in_seconds)
+        logger.info("Token with int uid and privilege: {}".format(token))
+
+        return token
