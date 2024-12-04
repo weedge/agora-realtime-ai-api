eca482086c7fdd61fa6550b9a37b1d44af1ee896
weedge <weege007@gmail.com>
2024-12-04 20:00:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.10',
     install_requires=[
         'pyee==12.0.0',
         'agora-python-server-sdk==2.0.8'
